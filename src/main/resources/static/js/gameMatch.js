f97380906cae304c8da5bf4e9965b9d764288bff
yesolje <70881757+yesolje@users.noreply.github.com>
2024-01-01 18:03:38 +0900

    iframe 코트 동적 생성

--- src/main/resources/static/js/gameMatch.js
+++ src/main/resources/static/js/gameMatch.js
@@ -49,6 +49,36 @@ var gameMatch={
             ],
         });
     },
+    addCourt:function(){
+       //코트추가 버튼을 눌러 코트를 추가한다.
+       //코트는 iframe 으로 이루어지며 id 값으로 고유 코트값을 갖는다.
+        var courtId = gameMatch.generateRandomCourtId();
+        console.log(courtId);
+
+        var iframe = document.createElement('iframe');
+        iframe.src = '/iframe/court.html'; // 로드할 외부 HTML 파일의 경로로 수정
+        //iframe.frameBorder = '0'; // 외곽선 제거
+        // iframe을 추가할 영역 가져오기
+        var container = document.querySelector('.container_gameMatch');
+
+        // iframe을 영역에 추가
+        container.appendChild(iframe);
+
+
+    },
+    deleteCourt:function(){
+
+    },
+    generateRandomCourtId: function(){
+      var characters ='1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+      var result = '';
+      var charactersLength = characters.length;
+      for (let i = 0; i < 20; i++) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+
+      return result;
+    },
 
 }
 
